5b9d026210d1ecf33f4a154ff306cc303d72810b
Lin, Cheng-Tao <mutolisp@gmail.com>
2022-09-04 00:18:02 +0800

    新增大專院校校務資訊公開資料

---
+++ data/udb/README.md
@@ -0,0 +1,2 @@
+資料來源：https://udb.moe.edu.tw/udata/
+爬取日期：2022-09-04
---
+++ data/udb/scraper/curl_研究類2016.sh
@@ -0,0 +1,5 @@
+curl "https://udb.moe.edu.tw/udata/Download/StaticDownload?FileType=csv&ReportID=SirdIndustryCooperativeResearchProjectRatio" -o "研1.學校承接各單位資助「各類計畫經費」及其每師平均承接金額-以「校」統計.csv"
+curl "https://udb.moe.edu.tw/udata/Download/StaticDownload?FileType=csv&ReportID=SirdIndustryCooperativeResearchProject" -o "研2.學校承接各單位資助「產學合作」計畫經費及其每師平均承接金額-以「校」統計.csv"
+curl "https://udb.moe.edu.tw/udata/Download/StaticDownload?FileType=csv&ReportID=SirdPatentAndAuthorizedCountRatio" -o "研3.學校申請專利、新品種及授權件數-以「校」統計.csv"
+curl "https://udb.moe.edu.tw/udata/Download/StaticDownload?FileType=csv&ReportID=SirdIntellectualPropertyUse" -o "研4.學校各種智慧財產權衍生運用總金額-以「校」統計.csv"
+curl "https://udb.moe.edu.tw/udata/Download/StaticDownload?FileType=csv&ReportID=SirdRegularTeacherSurveyProjectFunds" -o "研5.專任教師獲資助「學術研究」計畫經費及其每師平均承接金額-以「校」統計.csv"
